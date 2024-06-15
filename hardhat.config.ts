1f451af0cde99ea21b75ab4dbca06ea35e1261e2
Vince-maple-byte <73848683+Vince-maple-byte@users.noreply.github.com>
2024-06-15 01:42:18 -0400

    Added a gas reporter in hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,13 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-gas-reporter";
 import * as dotenv from "dotenv";
 dotenv.config();
 
 const SEPOLIA_URL = process.env.SEPOLIA_URL;
 const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const COINMARKET_API_KEY = process.env.COINMARKET_API_KEY;
 
 const config: HardhatUserConfig = {
     //defaultNetwork is always hardhat
@@ -27,6 +29,12 @@ const config: HardhatUserConfig = {
     solidity: "0.8.24",
     etherscan: {
         apiKey: ETHERSCAN_API_KEY!,
+    },
+    //This gives us an idea of the amount of gas/cost our smart contracts would spend from our tests
+    gasReporter: {
+        currency: "USD",
+        L1: "fantom",
+        coinmarketcap: COINMARKET_API_KEY!,
     },
 };
 
